2ea3b8cd6a486464c70d2c62dbfc49deeda79c05
Alexander Kohler <alex@Alexanders-Mac-mini.local>
2017-03-18 13:56:23 -0400

    initial commit

---
+++ startup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Variables
+HOSTNAME=$1
+USER=$2
+HOMEIP=$3
+
+if [ -z "${HOSTNAME}" ]; then
+    echo "hostname not set"
+	exit 1
+fi
+
+if [ -z "${USER}" ]; then
+    echo "user not set"
+	exit 1
+fi
+
+if [ -z "${HOMEIP}" ]; then
+    echo "home IP not set"
+	exit 1
+fi
+
+# update debian
+export DEBIAN_FRONTEND=noninteractive # don't bother me with prompts
+export APT_LISTCHANGES_FRONTEND=none
+apt-get -y update && apt-get -y upgrade
+
+# Update hostname
+sed -i "s/debian/$HOSTNAME/g" /etc/hosts
+hostnamectl set-hostname $HOSTNAME
+
+# set timezone - not really required, we'll just be in UTC, I'd rather not navigate through the GUI if I don't have to :)
+# dpkg-reconfigure tzdata
+
+# User creation
+apt-get install sudo
+adduser $USER
+adduser $USER sudo
+
+# SECURING DA SERVER 
+# disable root login via ssh
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+
+# listen to ssh connections only over ipv4
+echo 'AddressFamily inet' | sudo tee -a /etc/ssh/sshd_config
+
+# sync our changes
+systemctl restart sshd
+
+# install fail2ban
+apt-get install -y fail2ban
+
+# create local jail
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+# whitelist our home IP.
+sed -i "s/ignoreip = 127.0.0.1\/8/ignoreip = 127.0.0.1\/8 $HOMEIP/" /etc/fail2ban/jail.local
+
+# rest of the default configuration is good enough. Onward!
+
+# install our vnc server and window manager
+apt-get install -y vnc4server
+apt-get install -y xfce4 
+
+
+# install java
+apt-get install -y software-properties-common python-software-properties # so we can actually add-apt-repository
+add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
+apt-get update
+apt-get install -y oracle-java8-installer
+apt-get install -y git
+
+mkdir -p /home/$USER/.vnc
+rm /home/$USER/.vnc/xstartup
+ln -s /etc/X11/xinit/xinitrc /home/$USER/.vnc/xstartup
+chmod 755 /home/$USER/.vnc/xstartup
+chown -R $USER:$USER /home/$USER
+
+vncserver -localhost :1
